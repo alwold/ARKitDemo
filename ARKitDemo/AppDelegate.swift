ec6ab40aaf23855884f0340e17eeb0a93c9082e0
Al Wold <alwold@alwold.com>
2017-09-21 09:02:52 -0700

    initial empty project setup

---
+++ ARKitDemo/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  ARKitDemo
+//
+//  Created by Al Wold on 9/21/17.
+//  Copyright © 2017 Al Wold. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
