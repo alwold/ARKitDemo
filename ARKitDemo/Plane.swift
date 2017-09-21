9b1a1c79d60b937ffb058ea8812b3412d576cc08
Al Wold <alwold@alwold.com>
2017-09-21 09:40:00 -0700

    add plane detection and basic plane objects

---
+++ ARKitDemo/Plane.swift
@@ -0,0 +1,46 @@
+//
+//  Plane.swift
+//  ARTest
+//
+//  Created by Al Wold on 9/20/17.
+//  Copyright © 2017 Al Wold. All rights reserved.
+//
+
+import SceneKit
+import ARKit
+import os.log
+
+class Plane: SCNNode {
+    let plane: SCNBox
+    
+    init(anchor: ARPlaneAnchor) {
+        let width = CGFloat(anchor.extent.x)
+        let length = CGFloat(anchor.extent.z)
+        plane = SCNBox(width: width, height: 0.01, length: length, chamferRadius: 0.0)
+        
+        super.init()
+        
+        updateColor()
+        self.geometry = plane
+        self.position = SCNVector3(anchor.center.x, anchor.center.y, anchor.center.z)
+        self.physicsBody = SCNPhysicsBody(type: .kinematic, shape: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Don't use this initializer")
+    }
+    
+    func update(anchor: ARPlaneAnchor) {
+        os_log("updating plane")
+        let width = CGFloat(anchor.extent.x)
+        let length = CGFloat(anchor.extent.z)
+        plane.width = width
+        plane.length = length
+        self.position = SCNVector3(anchor.center.x, anchor.center.y, anchor.center.z)
+    }
+    
+    func updateColor() {
+        plane.firstMaterial!.diffuse.contents = UIColor(white: 1.0, alpha: 0.0)
+    }
+}
+
--- ARKitDemo/ViewController.swift
+++ ARKitDemo/ViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 import ARKit
+import os.log
 
 class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     @IBOutlet weak var sceneView: ARSCNView!
+    
+    var planesByAnchorIdentifier = [UUID: Plane]()
     
     // MARK: - View Controller lifecycle
     override func viewDidLoad() {
@@ -22,8 +25,39 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         super.viewWillAppear(animated)
         
         let configuration = ARWorldTrackingConfiguration()
-        
+        configuration.planeDetection = .horizontal
+
         sceneView.session.delegate = self
         sceneView.session.run(configuration)
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let anchor = anchor as? ARPlaneAnchor {
+            os_log("adding plane")
+            let plane = Plane(anchor: anchor)
+            node.addChildNode(plane)
+            planesByAnchorIdentifier[anchor.identifier] = plane
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let anchor = anchor as? ARPlaneAnchor {
+            os_log("got plane update")
+            if let plane = planesByAnchorIdentifier[anchor.identifier] {
+                os_log("found existing plane, updating it")
+                plane.update(anchor: anchor)
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        if let anchor = anchor as? ARPlaneAnchor {
+            os_log("plane removed by ARKit")
+            if let plane = planesByAnchorIdentifier.removeValue(forKey: anchor.identifier) {
+                os_log("found the plane, removing it")
+                plane.removeFromParentNode()
+            }
+        }
     }
 }
